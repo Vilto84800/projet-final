9c32aa38c52ac3bbd43a59b403a670c4a11ba6c1
Vilto84800 <145466329+Vilto84800@users.noreply.github.com>
2024-03-14 16:19:39 +0100

    Add files via upload

--- Projet final/script.js
+++ Projet final/script.js
@@ -7,12 +7,3 @@ function closeMenu() {
     document.getElementById('side-menu').style.width = '0';
     document.getElementById('main-content').style.marginLeft = '0';
 }
-
-function mobilecheck() {
-    var device = (typeof window.orientation !== "undefined") 
-        || (navigator.userAgent.indexOf('IEMobile') !== -1
-        );
-    if (device == true){
-        document.location.href="test.html"
-    }
-};
